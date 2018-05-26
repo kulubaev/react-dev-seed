4b0ea91419299e6a72de1d6c7d164e9dddede564
ruslan kulubaev <ruslan@ruslans-MBP.mshome.net>
2018-05-27 00:40:39 +0400

    code prettier config - added prettier support - prettified existing code

--- src/api/http.js
+++ src/api/http.js
@@ -2,21 +2,16 @@ import uri from './uri';
 
 const success = resp => {
   return resp.json();
-}
-const error = error => {
+};
 
+const error = error => {
   /*eslint-disable no-console*/
   console.error(error);
   /*eslint-enable no-console*/
-}
+};
 
 export default class {
-
-  static get(url){
-    return fetch(uri + url)
-      .then(success,error);
+  static get(url) {
+    return fetch(uri + url).then(success, error);
   }
 }
-
-
-
--- src/app/index.js
+++ src/app/index.js
@@ -3,21 +3,17 @@ import httpApi from '../api/http';
 console.log('starting app');
 
 httpApi.get('/users').then(response => {
-
   let result = '';
-
-  response.forEach( user => {
+  response.forEach(user => {
     result += `<tr>
-      <td>${user.id}</td>
-      <td>${user.firstName}</td>
-      <td>${user.lastName}</td>
-      <td>${user.email}</td>
-      </tr>`
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+      </tr>`;
   });
 
   global.document.getElementById('users').innerHTML = result;
-
 });
-
 
 /*eslint-enable no-console*/
